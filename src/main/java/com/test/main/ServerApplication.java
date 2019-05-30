a3ed119b0417ab73a9e0c61774a7943556b8bc93
githubforalpha <alpha_2016@qq.com>
2019-05-30 11:14:00 +0800

    test git

---
+++ src/main/java/com/test/main/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.test.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerApplication {
+//---
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
